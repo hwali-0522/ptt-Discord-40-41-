e2c714a63130ef93ae27da24156b1a2fef85bbc4
hwali-0522 <116364511+hwali-0522@users.noreply.github.com>
2022-12-16 22:42:20 +0800

    Update main.py

--- ptt爬蟲+Discord機器人/main.py
+++ ptt爬蟲+Discord機器人/main.py
@@ -31,7 +31,7 @@ async def on_message(message):#對話事件
         if message.content.lower() == 'test':
             await message.channel.send('bot.event check')
         if message.content.lower() == '.ptt':
-            await message.channel.send('ptt網址:\nhttps://www.ptt.cc/bbs/index.html\n完整的命令\n.ptt [看版名稱(英文)] [頁數(找最新的一頁輸入數字以外的東西)]')
+            await message.channel.send('ptt網址:\nhttps://www.ptt.cc/bbs/index.html\n完整的命令\n.ptt [看版名稱]')
     await bot.process_commands(message)#啟用指令command這個功能
 
 
@@ -39,31 +39,30 @@ async def on_message(message):#對話事件
 async def test(ctx):
     await ctx.send("cmd check")
 
-def ptt_spider(text, page_num):#ptt爬蟲副函式
+def ptt_spider(text):#ptt爬蟲副函式(爬特定版最新的一頁)
   
     result=''
     url = 'https://www.ptt.cc/'
-    try:
-        int(page_num)
-        web = requests.get(f'https://www.ptt.cc/bbs/{jf[text]}/index{page_num}.html', cookies={'over18': '1'})
-    except:
-        web = requests.get(f'https://www.ptt.cc/bbs/{jf[text]}/index.html', cookies={'over18': '1'})
 
+    web = requests.get(f'https://www.ptt.cc/bbs/{jf[text]}/index.html', cookies={'over18': '1'})
+        
     soup = BeautifulSoup(web.text, 'html5lib')
     titles = soup.find_all('div', class_='title')
-
-    for i in titles:
-        if i.find('a') != None:
-            result+=(i.find('a').get_text())
-            result+='\n'
-            result+=str(url+i.find('a')['href'])
-            result+='\n\n'
+    if len(titles) == 0:
+        result += '404 Not Found.'
+    else: 
+        for i in titles:
+            if i.find('a') != None:
+                result+=(i.find('a').get_text())
+                result+='\n'
+                result+=str(url+i.find('a')['href'])
+                result+='\n\n'
     return result
 
  
 @bot.command()#ptt爬蟲的指令
-async def ptt(ctx, text, page_num):
-    await ctx.send(ptt_spider(text.lower(), page_num))
+async def ptt(ctx, text):
+    await ctx.send(ptt_spider(text.lower()))
     
 
 
